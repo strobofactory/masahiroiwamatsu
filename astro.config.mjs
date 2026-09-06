30d59700312fdee61bd6b6b4e60028eee53b908d
strobofactory <info@strobofactory.net>
2026-09-06 18:45:08 +0900

    Use www as canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://masahiroiwamatsu.com',
+  site: 'https://www.masahiroiwamatsu.com',
   output: 'static',
   integrations: [sitemap()]
 });
